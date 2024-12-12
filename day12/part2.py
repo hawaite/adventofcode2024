a5d7a0232c34c42d6fabc0c4b5cb265447692978
Helier Waite <helierawaite@gmail.com>
2024-12-12 20:08:50 +0000

    day 12 part 2. severely reduced the lines of code by reducing down to a single corner checking function

--- day12/part2.py
+++ day12/part2.py
@@ -1,37 +1,22 @@
-from util.grids import Direction, Grid, Point, cardinal_directions
+from util.grids import Grid, Point, cardinal_directions, ordinal_directions
 
-# external corners like the 4 on the outside of a square. a 90 degree angle wrt the shape
-# a single point can be 4 external corners at once
-# a point counts as an external corner if we take 2 consecutive cardinal directions
-# and both of them dont match the current point value. Out of bounds counts as not matching
-def external_corners_on_point(point:Point, grid:Grid) -> int:
+def corners_on_point(point:Point, grid:Grid) -> int:
     total = 0
     current_point_value = grid.get_value(point)
-    directions_to_test = [Direction.NORTHEAST, Direction.SOUTHEAST, Direction.SOUTHWEST, Direction.NORTHWEST]
-    for direction_to_test in directions_to_test:
-        # get a list of the two points we're testing
-        test_points = [Point(point.col + direction_to_test.rotate(angle).value.col, point.row + direction_to_test.rotate(angle).value.row) for angle in [-45, 45]]
-        # get only points which are either out of bounds, or do NOT match the current point value
-        oob_or_not_matching_points = [tp for tp in test_points if not grid.position_in_bounds(tp) or (grid.position_in_bounds(tp) and grid.get_value(tp) != current_point_value)]
-        # if we still have two points then this is an external corner
-        if len(oob_or_not_matching_points) == 2:
-            total += 1
-    return total
+    directions_to_test = ordinal_directions
 
-# internal corners are like that one 270 degree angle on the inside of an "L" shape
-# a single point can be multiple external corners, as seen in the "E" example input
-# we test for this by taking three consecutive directional points and if the outer two match the the current point value
-# and the middle one does not, we have an internal corner. It is not possible for an internal corner to be out of bounds.
-def internal_corners_on_point(point:Point, grid:Grid) -> int:
-    # up & left must be value, while upleft is not
-    current_point_value = grid.get_value(point)
-    total = 0
-    directions_to_test = [Direction.NORTHEAST, Direction.SOUTHEAST, Direction.SOUTHWEST, Direction.NORTHWEST]
     for direction_to_test in directions_to_test:
         test_points = [Point(point.col + direction_to_test.rotate(angle).value.col, point.row + direction_to_test.rotate(angle).value.row) for angle in [-45, 0, 45]]
+
+        # test for external corner in direction
+        oob_or_not_matching_points = [tp for tp in [test_points[0], test_points[2]] if not grid.position_in_bounds(tp) or (grid.position_in_bounds(tp) and grid.get_value(tp) != current_point_value)]
+        if len(oob_or_not_matching_points) == 2:
+            total += 1
+            continue # cannot be both an internal and external corner in this one direction
+
+        # test for internal corner in direction
         if not all([grid.position_in_bounds(p) for p in test_points]):
             continue # this direction is not all inbounds, but others may be
-
         # if point 0 and 2 match but point 1 does not, thats an internal corner
         if grid.get_value(test_points[0]) == current_point_value and grid.get_value(test_points[2]) == current_point_value and grid.get_value(test_points[1]) != current_point_value:
             total += 1
@@ -44,12 +29,13 @@ def solve(lines:list[str]):
     total = 0
     for row in range(0,grid.height):
         for col in range(0,grid.width):
-            # for each point, attempt a flood-fill from there to get all the points
-            # replace those points in the grid with '.'
+            # for each point, attempt a flood-fill from there to get all the points in this group
             points_in_group = set()
             start_point = Point(col,row)
+            
             if start_point in all_consumed_points:
                 continue
+
             to_check = set([start_point])
             val_to_find = grid.get_value(start_point)
 
@@ -65,17 +51,12 @@ def solve(lines:list[str]):
             
             all_consumed_points = all_consumed_points.union(points_in_group)
 
-            # work out perimeter
-            external_corners = 0
-            internal_corners = 0
             area = len(points_in_group)
+
             # number of sides is equal to number of corners
-            for point in points_in_group:
-                # two types of corner with different rules for checking, but all count towards sides
-                external_corners += external_corners_on_point(point, grid)
-                internal_corners += internal_corners_on_point(point, grid)
+            corners = sum([corners_on_point(p, grid) for p in points_in_group])
             
-            print(f"val to find: {val_to_find}, area: {area}, external corners: {external_corners}, internal corners: {internal_corners}")
-            total += ((internal_corners + external_corners) * area)
+            # print(f"val to find: {val_to_find}, area: {area}, corners: {corners}")
+            total += (corners * area)
 
     print(total)
