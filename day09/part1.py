decf2ae492f5bf28ee00dc3158e2e5dc8cfaeed1
Helier Waite <helierawaite@gmail.com>
2024-12-14 22:26:54 +0000

    update day 9 part 1 to use a double pointer method rather than the list weirdness I was doing. runtime reduced from 4 seconds to 200ms

--- day09/part1.py
+++ day09/part1.py
@@ -14,34 +14,36 @@ def expand_disk_def(disk_def):
         is_free_space = not is_free_space
     return expanded_disk_def_parts
 
-def find_all_space_ordered(disk_def):
-    found_spaces = []
-
-    for ix, val in enumerate(disk_def):
-        if val == ".":
-            found_spaces.append(ix)
-    return found_spaces
-
 def checksum(disk_layout):
     total = 0
     for ix, val in enumerate(disk_layout):
         if val != ".":
             total += (ix * int(val))
     return total
+
 def solve(lines:list[str]):
     disk_layout = expand_disk_def(lines[0])
 
-    free_spaces = find_all_space_ordered(disk_layout)
-    for i in range(0,len(disk_layout)):
-        ix_item_to_move = len(disk_layout) -1 - i
-        if disk_layout[ix_item_to_move] == ".":
+    space_ptr = 0
+    file_ptr = len(disk_layout) - 1
+    
+    while space_ptr < file_ptr:
+        # space pointer currently on a file. increment and go again.
+        if disk_layout[space_ptr] != ".":
+            space_ptr += 1
             continue
-        else:
-            ix_earliest_free_space = free_spaces[0]
-            free_spaces = free_spaces[1:]
-            if ix_earliest_free_space > ix_item_to_move:
-                break # everything beyond here is free space
-            disk_layout[ix_earliest_free_space] = disk_layout[ix_item_to_move]
-            disk_layout[ix_item_to_move] = "."
+
+        # file pointer current on a space. increment and go again.
+        if disk_layout[file_ptr] == ".":
+            file_ptr -= 1
+            continue
+
+        # space pointer on a space. file pointer on a file
+        disk_layout[space_ptr] = disk_layout[file_ptr]
+        disk_layout[file_ptr] = "."
+
+        # roll both pointers as both values used
+        space_ptr += 1
+        file_ptr -= 1
 
     print(checksum(disk_layout))
