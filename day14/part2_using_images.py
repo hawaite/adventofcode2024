5da45f41b45103163b8df8dac31a8ada184c8fdd
Helier Waite <helierawaite@gmail.com>
2024-12-14 17:05:26 +0000

    moved the old day 14 image generation code to a new file and rewrote part 2 to use the variance function from pythons statistics module to work out the variance in x and y values each iteration.

--- day14/part2_using_images.py
+++ day14/part2_using_images.py
@@ -1,5 +1,7 @@
+# do not run unless you want to generate 10,403 PNGs in this directory
 from dataclasses import dataclass
-import statistics
+from PIL import Image
+import os
 
 @dataclass
 class Robot:
@@ -9,9 +11,10 @@ class Robot:
     speed_y: int
 
 def solve(lines:list[str]):
-    robots:list[Robot] = []
+    robots = []
     grid_width = 101
     grid_height = 103
+    os.mkdir("./day14/temp")
 
     for line in lines:
         position_part, speed_part = line.split(" ")
@@ -19,29 +22,18 @@ def solve(lines:list[str]):
         x_speed, y_speed = speed_part[2:].split(",")
         robots.append(Robot(int(x_pos), int(y_pos), int(x_speed), int(y_speed)))
 
+    # today, we're going to generate 101 * 103 PNG files 
+    # and then look through them in file explorer until we spot something looking like a christmas tree
     # after 101 * 103 ticks the robots will all be in their starting position
-    # so no need to continue beyond there
-    min_variance = -1
-    min_variance_ix = 0
     for tick in range(0,grid_width * grid_height):
+        img = Image.new("1", (grid_width, grid_height)) # 1 bpp black rectangle
         # move robots
         for robot in robots:
             new_x_pos = (robot.x + robot.speed_x) % grid_width
             new_y_pos = (robot.y + robot.speed_y) % grid_height
             robot.x = new_x_pos
             robot.y = new_y_pos
-
-        all_x_values = [robot.x for robot in robots]
-        all_y_values = [robot.y for robot in robots]
-        x_var = statistics.variance(all_x_values)
-        y_var = statistics.variance(all_y_values)
-        var = x_var * y_var
-
-        if var < min_variance or min_variance == -1:
-            # if we have a new smallest variance, or this is the first loop
-            min_variance = var
-            min_variance_ix = tick + 1
-
-    # really this is just the iteration that produced the lowest variance in x and y values
-    # but since the tree is just a cluster of close together points, it works for this input.
-    print(f"Tree is PROBABLY at tick: {min_variance_ix}")
+            img.putpixel((robot.x, robot.y), 1) # add white pixel where robot is
+        
+        # render image
+        img.save(f"./day14/temp/output_{tick+1}.png")
