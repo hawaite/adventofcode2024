d72eeed0fe6b9e1e8691b19640545a056b37f9f8
Helier Waite <helierawaite@gmail.com>
2024-12-14 23:45:39 +0000

    day 9 part 2 update to move to managing lists of spans for the files and spaces. Now takes 6 seconds (or 1.2 in pypy3) instead of 3 minutes. Still higher than I'd like, entirely caused by sorting the spaces list when not required. If we inserted the space in the correct space then we wouldnt need to sort at all.

--- day09/part2.py
+++ day09/part2.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from collections import deque
+from typing import Dict, Tuple
 
 @dataclass
 class Span:
@@ -6,23 +8,7 @@ class Span:
     length:int
 
 #takes a disk definition and fully expands it.
-def expand_disk_def_list(disk_def):
-    digits = [int(x) for x in list(disk_def)]
-    is_free_space = False
-    expanded_disk_def_parts = []
-    file_id = 0
-    for i in range(0, len(digits)):
-        if is_free_space:
-            expanded_disk_def_parts.extend("."*digits[i])
-        else:
-            for j in range(0,digits[i]):
-                expanded_disk_def_parts.append(str(file_id))
-            file_id += 1
-        is_free_space = not is_free_space
-    return expanded_disk_def_parts
-
-#takes a disk definition and fully expands it.
-def get_files_and_spaces(disk_def):
+def get_files_and_spaces(disk_def) -> Tuple[Dict[str, Span], list[Span]]:
     digits = [int(x) for x in list(disk_def)]
     is_free_space = False
     files = {}
@@ -41,37 +27,64 @@ def get_files_and_spaces(disk_def):
         is_free_space = not is_free_space
     return (files, spaces)
 
-def checksum(disk_layout):
+def checksum_from_file_dict(file_dict:Dict[str, Span]):
     total = 0
-    for ix, val in enumerate(disk_layout):
-        if val != ".":
-            total += (ix * int(val))
+    for file_id, file_span in file_dict.items():
+        for i in range(file_span.start, file_span.start + file_span.length):
+            total += (i * file_id)
     return total
 
+# this will order the remaining space and consolidate contiguous blocks of space in to a single space
+def order_and_consolidate_space(spaces:list[Span]) -> list[Span]:
+    sorted_spaces = sorted(spaces, key=lambda space_span : space_span.start) 
+    i = 0
+    while True:
+        if i == len(sorted_spaces) - 1:
+            break # at last element, meaning cant get an i+1
+        space_one = sorted_spaces[i]
+        space_two = sorted_spaces[i+1]
+        if space_one.start + space_one.length == space_two.start:
+            # these two can be combined
+            # update the first one and delete the second one
+            space_one.length = space_one.length + space_two.length
+            del sorted_spaces[i+1]
+            # dont increment i. the new value at i may be combinable with i + 1
+        else:
+            # cant combine these two so move on
+            i += 1
+
+    return sorted_spaces
+
 def solve(lines:list[str]):
-    disk_layout = expand_disk_def_list(lines[0])
-    print(disk_layout)
     files, spaces = get_files_and_spaces(lines[0])
-    print(f"files: {files}")
-    print(f"spaces: {spaces}")
+    file_keys = deque(files.keys())
 
-    file_id_lst = list(reversed(files.keys()))
-    total_files = len(file_id_lst)
-    for file_id in list(reversed(files.keys())):
-        print(f"{file_id} / {total_files}")
-        index_to_inspect = 0
-        while index_to_inspect < files[file_id].start: #only check locations before current position
-            if disk_layout[index_to_inspect:index_to_inspect+files[file_id].length] == list("." * files[file_id].length):
-                # move to new location
-                for i in range(index_to_inspect, index_to_inspect+files[file_id].length):
-                    disk_layout[i] = file_id
-                #overwrite old location
-                for i in range(files[file_id].start, files[file_id].start+files[file_id].length):
-                    disk_layout[i] = "."
-                break # can place thing here and start next file
-            index_to_inspect += 1
-        # print(f"after: {disk_layout}")
+    while len(file_keys) > 0:
+        file = files[file_keys.pop()]
+        # iterate all spaces to see if theres somewhere that works
+        for space in spaces:
+            if space.start > file.start:
+                break # bail if space is later than the original space
 
-    # print(f"final layout: {disk_layout}")
+            if space.length == file.length:
+                original_file_start = file.start
+                file.start = space.start
+                space.start = original_file_start
+                spaces = order_and_consolidate_space(spaces)
+                break # file moved.
+            elif space.length > file.length:
+                # space bigger than file.
+                original_file_start = file.start
+                # place file in space
+                file.start = space.start
+                # reduce space that was there and shuffle it to end of what was just added
+                space.start = space.start + file.length
+                space.length = space.length - file.length
 
-    print(checksum(disk_layout))
+                # add new space where the file used to be
+                new_space = Span(original_file_start, file.length)
+                spaces.append(new_space)
+                spaces = order_and_consolidate_space(spaces)
+                break # file moved
+
+    print(checksum_from_file_dict(files))
